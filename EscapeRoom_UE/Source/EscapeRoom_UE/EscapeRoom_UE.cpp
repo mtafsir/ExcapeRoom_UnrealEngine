d9131664e862369b5d1d9073d7c5f2aba7901ac5
mtafsir <mtafsir95@gmail.com>
2019-04-21 16:21:50 -0500

    Initial Commit

---
+++ EscapeRoom_UE/Source/EscapeRoom_UE/EscapeRoom_UE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EscapeRoom_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeRoom_UE, "EscapeRoom_UE" );
